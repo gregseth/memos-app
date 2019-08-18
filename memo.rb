1e0c3fb54286781bc206af4edcd601a106027fda
Grégory Millasseau <greg.seth@gmail.com>
2019-08-18 23:20:26 +0200

    relocating relative paths

--- memo.rb
+++ memo.rb
@@ -5,7 +5,7 @@ class Memo
                                                                                                      
     DefaultPage = 'dev'                                                                              
     AllowedExtensions = { text: 'txt', markdown: 'md' }                                        
-    BaseDir = 'memos'                           
+    BaseDir = '../memos'
                                                                        
     def initialize(name)   
         # The dev page is the default                                                                          
